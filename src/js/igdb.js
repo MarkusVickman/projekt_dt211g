3b72e09d242f8bf740b8ee709d4ad3812587f017
Markus <markus.vickman@hotmail.com>
2024-02-28 12:24:35 +0100

    apin hämtar nu in toplista, rea, nya och kommande spel

--- src/js/igdb.js
+++ src/js/igdb.js
@@ -1,29 +1,37 @@
 
 
-let searchBtn = document.getElementById("searchbtn");
+let searchReview = document.getElementById("searchreview");
 let topList = document.getElementById("toplist");
-let picturesEl = document.getElementById("pictures");
+let comingSoon = document.getElementById("comingsoon");
 let buildReview = document.getElementById("review");
 let saleEl = document.getElementById("sale");
-let build = document.getElementById("");
+let newReleases = document.getElementById("newreleases");
 
 //vid click på sökknapp samlas input in och skickas vidare till apilänken
 topList.addEventListener("click", function (e) {
-  urlTop = 'https://steam-store-data.p.rapidapi.com/api/featuredcategories/';
-  steam("top_sellers", urlTop);
+  steam("top_sellers");
 })
 
 //vid click på sökknapp samlas input in och skickas vidare till apilänken
-sale.addEventListener("click", function (e) {
+saleEl.addEventListener("click", function (e) {
+  steam("specials");
+})
 
-  steam("specials", url);
+//vid click på sökknapp samlas input in och skickas vidare till apilänken
+newReleases.addEventListener("click", function (e) {
+  steam("newReleases");
+})
+
+//vid click på sökknapp samlas input in och skickas vidare till apilänken
+comingSoon.addEventListener("click", function (e) {
+  steam("comingSoon");
 })
 
 import { okey } from "./start";
 const okej = okey();
 
-async function steam(name, urlApi) {
-  const url = urlApi;
+async function steam(category) {
+  const url = 'https://steam-store-data.p.rapidapi.com/api/featuredcategories/';
   const options = {
     method: 'GET',
     headers: {
@@ -37,15 +45,36 @@ async function steam(name, urlApi) {
     const result = await response.json();
     console.log(result);
     //review(result);
-    buildPage(result);
+    if (category === "top_sellers"){
+    buildTopList(result);
+  }
+  else if(category === "specials"){
+    buildSpecials(result);
+  }
+
+  else if(category === "comingSoon"){
+    buildComingSoon(result);
+  }
+
+  else if(category === "newReleases"){
+    buildNewReleases(result);
+  }
+  
   } catch (error) {
     console.error(error);
   }
 
 }
 
-function buildPage(data) {
+function buildTopList(data) {
+
   let steamDeck = "";
+
+  let text = document.createTextNode(data.top_sellers.name);
+  let h2 = document.createElement("H2");
+  h2.appendChild(text);
+  buildReview.appendChild(h2);
+
   for (let i = 0; i < (data.top_sellers.items.length); i++) {
 
     if (data.top_sellers.items[i].name !== "Steam Deck") {
@@ -63,8 +92,49 @@ function buildPage(data) {
   }
 };
 
+function buildSpecials(data) {
+
+  let text = document.createTextNode(data.specials.name);
+  let h2 = document.createElement("H2");
+  h2.appendChild(text);
+  buildReview.appendChild(h2);
+
+  for (let i = 0; i < (data.specials.items.length); i++) {
+      let objectID = data.specials.items[i];
+      builder(objectID);
+  }
+};
+
+function buildNewReleases(data) {
+
+  let text = document.createTextNode(data.new_releases.name);
+  let h2 = document.createElement("H2");
+  h2.appendChild(text);
+  buildReview.appendChild(h2);
+
+  for (let i = 0; i < (data.new_releases.items.length); i++) {
+      let objectID = data.new_releases.items[i];
+      builder(objectID);
+  }
+};
+
+function buildComingSoon(data) {
+
+  let text = document.createTextNode(data.coming_soon.name);
+  let h2 = document.createElement("H2");
+  h2.appendChild(text);
+  buildReview.appendChild(h2);
+
+  for (let i = 0; i < (data.coming_soon.items.length); i++) {
+      let objectID = data.coming_soon.items[i];
+      builder(objectID);
+
+  }
+};
+
 
 function builder(objectID) {
+
   let gameHeader = document.createTextNode(objectID.name);
   //let text = document.createTextNode(top.top_sellers.items[i].large_capsule_image);
   let h3 = document.createElement("H3");
@@ -87,60 +157,8 @@ function builder(objectID) {
   article.appendChild(h3);
   buildReview.appendChild(article);
 
-
-
-  
-
   container.title = objectID.id;
 }
-
-
-//console.log(top.top_sellers.items.length);
-
-/*let header = document.createTextNode("Bokningsnr: " + key);
-let ul = document.createElement("ul");
-let h3 = document.createElement("h3");
-h3.appendChild(header);
-ul.appendChild(h3);
-//Varje "key-värdes array" loopas igenom och en lista byggs upp.
-for (let i = 0; i < value.length; i++) {
-    let li = document.createElement("li");
-    let text = document.createTextNode(value[i]);
-    li.appendChild(text);
-    ul.appendChild(li);
-}
-
-//Knappar skapas för att avboka eller omboka resa. Även här sparas bokningsnummret in som titel för att senare kunna tabort rätt resa.
-let cancel = document.createElement("button");
-cancel.classList.add("cancel_btn");
-cancel.title = key;
-let cancelText = document.createTextNode("Avboka");
-let rebook = document.createElement("button");
-rebook.title = key;
-rebook.href = "book_travel.html";
-rebook.classList.add("rebook_btn");
-let rebookText = document.createTextNode("Omboka resa");
-cancel.appendChild(cancelText);
-rebook.appendChild(rebookText);
-cancel.classList.add("red");
-//här skapas en container för varje bokning inklusive knappar och rubrik som skapas.
-let div = document.createElement("div");
-div.classList.add("box_auto_height");
-div.classList.add("lightgreen_border");
-div.appendChild(ul);
-div.appendChild(cancel);
-div.appendChild(rebook);*/
-
-/*För att lägga bokningen till rätt färdig element på sidan startar bokningsnummret(=key) på olika bokstäver och det kontrolleras med if och else if.*/
-/*if (key.charAt(0) == "t") {
-    writeTravel.appendChild(div);
-}
-
-else if (key.charAt(0) == "r") {
-    writeRecurring.appendChild(div);
-}
-}
-}*/
 
 
 async function review(games) {
